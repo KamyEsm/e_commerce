f016055b4875da6836d53e0fd06c935709c54440
kamy <130361402+KamyEsm@users.noreply.github.com>
2026-07-22 20:35:03 +0330

    refactor Carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,50 @@
 import Header from "@/components/Header";
 import HeroCarousel from "@/components/HeroCarousel";
 import Timer from "@/components/Timer";
-import {FlashSalesCarousel} from "@/components/FlashSalesCarousel";
+import {ShopCarousel} from "@/components/ShopCarousel";
+import SectionTag from "@/components/SectionTag";
+import {fetchActiveCampaigns} from "@/actions/campaignAction";
+import * as React from "react";
+import ProductCard from "@/components/ProductCard";
 
-export default function Home() {
-  return (
-      <main className="">
-          <Header/>
-          <HeroCarousel/>
-          <FlashSalesCarousel/>
-      </main>
-  );
+type CampaignsActionResponse = Awaited<ReturnType<typeof fetchActiveCampaigns>>;
+
+export type ActiveCampaignType = NonNullable<CampaignsActionResponse["data"]>[number];
+
+
+export default async function Home() {
+
+    //first campaign
+    const campaigns = await fetchActiveCampaigns();
+    let activeCampaign: ActiveCampaignType | null = null;
+    if(campaigns && campaigns.data && campaigns.success){
+        activeCampaign = campaigns.data[0];
+    }
+    else {
+        console.error("Campaign not found!");
+    }
+
+    //category
+
+    return (
+        <main className="">
+            <Header/>
+            <HeroCarousel/>
+            <ShopCarousel title={
+                <>
+                    <SectionTag title={"Today's"}/>
+                    <div className="flex flex-row justify-between gap-12 m-4">
+                        <h2 className="font-bold ">{activeCampaign?.title}</h2>
+                        <Timer targetDate={new Date(activeCampaign?.endDate || new Date())}/>
+                    </div>
+                </>
+            } activeCampaign={activeCampaign} renderFunction={(item) => (
+                <div className="p-1">
+                    <ProductCard productName={item.product.name} price={item.product.price}
+                                 discount={item.discountRate} imageUrl={item.product.ImageURL || "/image/no-image.png"} />
+                </div>
+            )}/>
+            <hr className="w-full border-t border-gray-200 my-16" />
+        </main>
+    );
 }
--- components/ShopCarousel.tsx
+++ components/ShopCarousel.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 
-import { Card, CardContent } from "@/components/ui/card"
 import {
     Carousel,
     CarouselContent,
@@ -8,39 +7,31 @@ import {
     CarouselNext,
     CarouselPrevious,
 } from "@/components/ui/carousel"
-import {fetchActiveCampaigns} from "@/actions/campaignAction";
 import ProductCard from "@/components/ProductCard";
-import Timer from "@/components/Timer";
-import SectionTag from "@/components/SectionTag";
 import Link from "next/link";
+import {ActiveCampaignType} from "@/app/page"
 
-export async function FlashSalesCarousel() {
-
-    const campaigns = await fetchActiveCampaigns();
+type ProductOnCampaignType = ActiveCampaignType["productsOnCampaigns"][number]
 
 
-    if (!campaigns.success || !campaigns.data || campaigns.data.length === 0) {
+export function ShopCarousel({title , activeCampaign , renderFunction}:{title:React.ReactNode , activeCampaign:ActiveCampaignType | null , renderFunction: (item: ProductOnCampaignType) => React.ReactNode;}) {
+
+    if(!activeCampaign){
         return <div className="text-center p-4">There are no campaigns to display.</div>;
     }
 
-    const activeCampaign = campaigns.data[0]
-
     return (
         <div>
-            <SectionTag title={"Today\'s"}/>
-            <div className="flex flex-row justify-between gap-12 m-4">
-                <h2 className="font-bold ">{activeCampaign.title}</h2>
-                <Timer targetDate={new Date(activeCampaign.endDate)} />
-            </div>
+            {title}
             <Carousel className="w-full">
                 <CarouselContent>
                     {activeCampaign.productsOnCampaigns.map((campaign,index) => (
                         <CarouselItem key={index}
                                       className="pl-4 basis-[82%]">
-                            <div className="p-1">
-                                <ProductCard productName={campaign.product.name} price={campaign.product.price}
-                                             discount={campaign.discountRate} imageUrl={campaign.product.ImageURL || "/image/no-image.png"} />
-                            </div>
+
+                            {
+                                renderFunction(campaign)
+                            }
                         </CarouselItem>
                     ))}
                 </CarouselContent>
